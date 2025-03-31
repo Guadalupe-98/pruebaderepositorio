a156a587230b49a120555405ab7acb8b9ddb5bd2
Guadalupe Rodriguez <guada.aybar.ga@gmail.com>
2025-03-30 21:22:04 -0300

    practica de introduccion js

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+const saludo = document.getElementById("saludo")
+saludo.textContent="hola puto el que lea";
+
+let number;
+number= 1;
+console.log(number)
+
+let string;
+string="baby";
+console.log(string);
+
+let boolean;
+boolean = false;
+console.log(boolean)
